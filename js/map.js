195735dc8e19c39d7bdf029b042816cc93931172
NikaCrimea <veronika74069@yandex.ru>
2018-12-19 23:13:03 +0300

    Добавила валидацию на поля кол-во комнат/кол-во гостей

--- js/map.js
+++ js/map.js
@@ -259,4 +259,58 @@ var getActiveState = function () {
 mainPin.addEventListener('mouseup', function () {
   getActiveState();
 });
+/**
+ *
+ *
+ */
+var roomNumber = document.querySelector('#room_number');
+var capacity = document.querySelector('#capacity');
+
+function CustomValidation() {
+  this.invalidities = [];
+}
+
+CustomValidation.prototype = {
+
+  checkValidity: function () {
+    var roomNumberValue = Number(roomNumber.value);
+    var capacityValue = Number(capacity.value);
+
+
+    if (capacityValue === 0 && roomNumberValue !== 100) {
+      this.addInvalidity('не для гостей');
+    }
+    if (capacityValue > roomNumberValue) {
+      this.addInvalidity('недопустимое количесвто гостей');
+    }
+    if (this.invalidities.length > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  },
+
+  addInvalidity: function (message) {
+    this.invalidities.push(message);
+  },
+
+  getInvalidities: function () {
+    return this.invalidities.join('. \n');
+  }
+};
+var selects = [roomNumber, capacity];
+
+selects.forEach(function (el) {
+
+  el.addEventListener('change', function () {
+
+    roomNumber.setCustomValidity('');
+
+    var validation = new CustomValidation();
+    validation.checkValidity(roomNumber, capacity);
+    var validityMessage = validation.getInvalidities();
+    roomNumber.setCustomValidity(validityMessage);
+
+  });
+});
 
